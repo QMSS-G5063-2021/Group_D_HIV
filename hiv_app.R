8b0500583325e6bcd6f7430940e2fcc2011cf50b
ZehuiWang <zw2525@columbia.edu>
2021-04-19 22:08:02 -0400

    lastest version

--- hiv_app.R
+++ hiv_app.R
@@ -87,32 +87,42 @@ ui <- fluidPage(
   
 #-------------------------------------------------------------------------  
   h2("Introduction"), 
-    p(a(href = "https://ourworldindata.org/hiv-aids#the-global-distribution-of-deaths-from-hiv-aids", 
-        "Data Source (ourworldindata.org)")),
-    p("An infection with HIV (human immunodeficiency virus) can lead to AIDS (acquired immunodeficiency syndrome). AIDS results in a gradual and persistent decline and failure of the immune system, resulting in heightened risk of life-threatening infection and cancers. In the majority of cases, HIV is a sexually-transmitted infection. However, HIV can also be transmitted from a mother to her child, during pregnancy or childbirth, or through breastfeeding. Non-sexual transmission can also occur through the sharing of injection equipment such as needles."),
-  
+    p("An infection with HIV (human immunodeficiency virus) can lead to AIDS (acquired immunodeficiency syndrome). 
+      AIDS results in a gradual and persistent decline and failure of the immune system, resulting in heightened risk of life-threatening infection and cancers. 
+      In the majority of cases, HIV is a sexually-transmitted infection. However, HIV can also be transmitted from a mother to her child, during pregnancy or childbirth, or through breastfeeding. 
+      Non-sexual transmission can also occur through the sharing of injection equipment such as needles."),
+      
+    p("HIV/AIDS is one of the world’s most fatal infectious diseases – 
+      particularly across Sub-Saharan Africa, where the disease has had a massive impact on health outcomes and life expectancy in recent decades."),
+
+    p("A couple of decades ago, the chances of surviving more than ten years with HIV were slim. Today, thanks to antiretroviral therapy (ART), people with HIV/AIDS can expect to live long lives. 
+      ART is a mixture of antiviral drugs that are used to treat people infected with human immunodeficiency virus (HIV). 
+      ART is an essential player in making progress against HIV/AIDS because it saves lives, allows people with HIV to live longer, and prevents new HIV infections."),
+   
     img(src = "WHO-HIV-Data.jpg", height = 450, width = 900),
     br(), br(),
 
   h3("This is an R Shiny interface to provide worldwide HIV related insights"),
-    p("HIV/AIDS is one of the world’s most fatal infectious diseases – particularly across Sub-Saharan Africa, where the disease has had a massive impact on health outcomes and life expectancy in recent decades.
-    
-    The Global Burden of Disease is a major global study on the causes of death and disease published in the medical journal The Lancet.1 These estimates of the annual number of deaths by cause are shown here. This chart is shown for the global total, but can be explored for any country or region using the “change country” toggle.
-    
-    In the chart we see that, globally, it is the second most fatal infectious disease.
-    
-    According to the Global Burden of Disease study, almost one million (954,000) people died from HIV/AIDS in 2017. To put this into context: this was just over 50% higher than the number of deaths from malaria in 2017.
-    
-    It’s one of the largest killers globally; but for some countries – particularly across Sub-Saharan Africa, it’s the leading cause of death. If we look at the breakdown for South Africa, Botswana or Mozambique – which you can do on the interactive chart – we see that HIV/AIDS tops the list. For countries in Southern Sub-Saharan Africa, deaths from HIV/AIDS are more than 50% higher than deaths from heart disease, and more than twice that of cancer deaths."),
+  h4("There are four sections: 
+     1)worldwide distribution of HIV, 
+     2) worldwide death caused by HIV, 
+     3) prevention of HIV and 
+     4) HIV related tweet text analysis."),
+     
 
 #------------------------------------------------------------------------- 
   h2("HIV Worldwide Distribution"),
   h4("Share of Population Infected with HIV"),    
-    p("Between 1996 and 2001 more than 3 million people were infected with HIV ever year. Since then the number of new infections began to decline and in 2017 it was reduced to below 2 million. The lowest number of new infections since 1990."),
+    p("From the interactive plot of Share of population infected with HIV, we can clearly identify the trends of spreading of HIV. 
+      In the 90s, share of infected population was comparably lower, with only 2 countries exceeding 10% (Uganda 14.21% and Zimbabwe 14.12%) The distribution of high density of HIV generally located at the middle-south part of Africa.
+      "),
+    p("Around 2000, the southern part suffered most from HIV population proportion: multiple countries exceeded 10% share of infected population with the highest being Botswana, 26.67%
+      The HIV share in Africa reached its peak at 2004. Then HIV began to spread to other continents at a quicker pace. For example, share of population in Russia was 0.2% in 2004, and went all the way up to 1.36% in 2017. Many other regions such as South-eastern Asia and South America also appeared to have increased share of infected population, proving that the spread of HIV is speeding up.
+      "),
     #create a slider with year in data range (1990-2017)
     sliderInput(inputId = "year1",
                 label = "Choose a year",
-                value = 2017, min = 1990, max = 2017, width = "500px"),
+                value = 2017, min = 1990, max = 2017, width = "700px"),
     
     #plot map
     plotlyOutput(outputId = "share_map",
@@ -121,10 +131,14 @@ ui <- fluidPage(
 
 #-------------------------------------------------------------------------   
   h4("Number of New Infections Each Year"),
+    p("Combined with previous graphs, it is not hard for us to infer that the newly diagnosed HIV cases has been increasing, too. However, since this plot focuses on the magnitude of new infections, it shows some interesting patterns not obvious in other graphs: For example, in terms of the absolute number of infection cases, countries with higher population such as United States, China, India and Brazil played significant role on the world stage. 
+       "),
+       p("Also, through the choropleth map we can see that the new infection cases have not always been increasing. One possible explanation is that some effective treatment and preventions may help keep the new infection numbers under control.
+        "),
     #create a slider with year in data range (1990-2017)
     sliderInput(inputId = "year2",
                 label = "Choose a year",
-                value = 2017, min = 1990, max = 2017, width = "500px"),
+                value = 2017, min = 1990, max = 2017, width = "700px"),
     
     #plot map
     plotlyOutput(outputId = "newcase_map",
@@ -134,29 +148,36 @@ ui <- fluidPage(
 #-------------------------------------------------------------------------    
   h2("HIV is fatal & Serious"), 
   h4("Share of Population Death because of HIV"),
-    p("Globally, 1.7% of deaths were caused by HIV/AIDS in 2017.This share is high, but masks the wide variations in the toll of HIV/AIDS across the world. In some countries, this share was much higher."),
-    p("In the interactive map we see the share of deaths which resulted from HIV/AIDS across the world. 
-        Across most regions the share was low: across Europe, for example, it accounted for less than 0.1% of deaths.
+    p("Globally, 1.7% of deaths were caused by HIV/AIDS in 2017. 
+      This share is high already, but in some countries, this share was much higher."),
+    p("In the interactive map we can see the share of deaths which resulted from HIV/AIDS across the world. 
+        Across most regions the share was low, for example Europe, it accounted for less than 0.1% of deaths.
         But across some countries – focused primarily in Southern Sub-Saharan Africa – the share is very high. 
         More than 1-in-4 of deaths (28%) in South Africa and Botswana were caused by HIV/AIDS in 2017. 
         The share was also very high across Mozambique (24%); Namibia (23%); Zambia (18%); Kenya (17%); and Congo (15%)."),
+    p("If we use the slider and choose a different year as the input, 
+       we can see that the share of death increased significantly since 1994, in some countries the share of death even reached 60%.
+       The upward trend did not stop until the Year 2006, and agter Year 2006, the death share fianlly began to decrease."),
     
     sliderInput(inputId = "year",
                 label = "Choose a year",
                 value = 2000, 
                 min = 1990, 
-                max = 2017),
+                max = 2017,
+                width = "700px"),
     
     plotlyOutput(outputId = "death_share_map",
                  height = "500px",
                  width = "900px"),
 
 #-------------------------------------------------------------------------
-  h4("Death Rate among the Top 8 HIV severe Countries"),
-    p("If we take a furthur look at the Death Rate of HIV, we could sadly see that for the Top 8 countries, 
-      with highest HIV death rate, the death rate in these countries is increasing gradually.
-      The large health burden of HIV/AIDS across Sub-Saharan Africa is also reflected in death rates. 
-      Death rates measure the number of deaths from HIV/AIDS per 100,000 individuals in a country or region."),
+  h4("Death Rate among the most Severly HIV Infected Countries"),
+    p("The large health burden of HIV/AIDS across Sub-Saharan Africa is also reflected in death rates. 
+      Death rates measure the number of deaths from HIV/AIDS per 100,000 individuals in a country or region.
+      If we take a furthur look at the Death Rate of HIV, 
+      we could sadly see that for Turkey, Maldives and Comoros, the death rate of HIV increases with time; 
+      while for many other countries, the death rate of HIV is under controlled gradually.
+      "),
       plotlyOutput(outputId = "death_rate_line",
                    height = "500px",
                    width = "900px"),
@@ -173,11 +194,13 @@ ui <- fluidPage(
 #------------------------------------------------------------------------- 
   h2("HIV Can Be Prevented "), 
   h4("Antiretroviral Therapy Coverage of Infected Population"), 
-    p("A couple of decades ago, the chances of surviving more than ten years with HIV were slim. Today, thanks to antiretroviral therapy (ART), people with HIV/AIDS can expect to live long lives. 
-      ART is a mixture of antiviral drugs that are used to treat people infected with human immunodeficiency virus (HIV). ART is an essential player in making progress against HIV/AIDS because it saves lives, allows people with HIV to live longer, and prevents new HIV infections. "),
+    p("A couple of decades ago, the chances of surviving more than ten years with HIV were slim. 
+      Today, thanks to antiretroviral therapy (ART), people with HIV/AIDS can expect to live long lives. 
+      ART is a mixture of antiviral drugs that are used to treat people infected with human immunodeficiency virus (HIV). 
+      ART is an essential player in making progress against HIV/AIDS because it saves lives, allows people with HIV to live longer, and prevents new HIV infections. "),
     sliderInput(inputId = "year_choose",
                 label = "Choose a year",
-                value = 2017, min = 2000, max = 2017, width = "500px"),
+                value = 2017, min = 2000, max = 2017, width = "700px"),
     
     plotlyOutput(outputId = "art_map",
                  height = "500px",
@@ -192,11 +215,28 @@ ui <- fluidPage(
 
 #------------------------------------------------------------------------- 
   h2("HIV related Text Analysis"),
+    p("By using the Twitter API, we got 1000 'HIV' related tweets, 
+      we cleaned it by removing non-English characters, remove stopwords... 
+      and finally got a DocumentTermMatrix of the HIV-related tweets for plotting the word cloud.
+      From the word cloud, we are able to see that 'amp','liberng' and 'testing' are the most frequently mentioned words with HIV content."),
+
     plotOutput(outputId = "wordcloud",
                height = "400px",
                width = "700px"),
-)
+#------------------------------------------------------------------------- 
+  h2("Conlusion"),
+     p("The world has made significant progress against HIV/AIDS. 
+     Global deaths from AIDS have halved over the past decade. HIV/AIDS once accounted for more than 1-in-3 deaths in some countries, but rates are now falling.
+     "),
+     p("Global progress on HIV/AIDS has been driven by large improvements in countries which were most affected by the HIV epidemic. We saw that over the past decade the death rate has fallen as antiretroviral treatment has become more widely available, and therefore, we optimistically expect that the death rate of HIV would continue to decrease in the next decades. 
+       More and more people are going to live without worry about being infected."),
 
+ h2("Reference"),
+ p(a(href = "https://ourworldindata.org/hiv-aids#the-global-distribution-of-deaths-from-hiv-aids", 
+     "HIV Data Source we used(ourworldindata.org)"))
+
+)
+#------------------------------------------------------------------------- 
 server <- function(input, output){
   #output for share map
   output$share_map<- renderPlotly({
@@ -213,7 +253,7 @@ server <- function(input, output){
     )
     fig <- plot_geo(sharemap_year)
     fig <- fig %>% add_trace(
-      z = ~Prevalence, color = ~Prevalence, colors = 'Reds',
+      z = ~Prevalence, color = ~Prevalence, colors = 'Blues',
       text = ~Entity, locations = ~Code, marker = list(line = l)
     )
     fig <- fig %>% colorbar(title = 'Share of infected population', ticksuffix = '%', thickness = 15)
@@ -238,7 +278,7 @@ server <- function(input, output){
     )
     fig <- plot_geo(newcasemap_year)
     fig <- fig %>% add_trace(
-      z = ~Incidence, color = ~Incidence, colors = 'Greens',
+      z = ~Incidence, color = ~Incidence, colors = 'Blues',
       text = ~Entity, locations = ~Code, marker = list(line = l)
     )
     fig <- fig %>% colorbar(title = 'Number of New Infections Each Year', thickness = 15)
@@ -285,7 +325,7 @@ server <- function(input, output){
            aes(x = Year, y = all, colour = Entity)) + 
       geom_line() +
       geom_point() +
-      labs(title="Top 8 Death Rate Countries",
+      labs(title="Top 8 Counties with Most Severly HIV Death Rates",
            x="Year",
            y="Death Rate of the country",
            color = "Country",
@@ -329,7 +369,7 @@ server <- function(input, output){
     wordcloud(rownames(freq_s), 
               freq_s[,1], 
               max.words=100, 
-              colors=brewer.pal(4, "Dark2"))
+              colors=brewer.pal(8, "Dark2"))
   })
   
   output$art_map<- renderPlotly({
@@ -361,7 +401,14 @@ server <- function(input, output){
   output$educ_graph<- renderPlot({
     
     fig <- ggplot(educ, aes(Entity, Knowledge)) + 
-      geom_col(aes(fill=Knowledge))
+      geom_col(aes(fill=Knowledge)) +
+      labs(title="HIV Education",
+           x="country",
+           y="HIV education coverage rate",
+           color = "Country",
+           caption = "Source: https://ourworldindata.org/hiv-aids#the-global-distribution-of-deaths-from-hiv-aids") +
+      theme(legend.position = 'top', 
+            plot.title = element_text(colour = alpha('black')))
     fig
     
   })
